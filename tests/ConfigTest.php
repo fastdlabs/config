81aabbe46411b7e87a2595058f9d1dd7368bf7d7
Runnerlee <runnerleer@gmail.com>
2017-05-10 18:32:58 +0800

    fix has config and upgrade set config

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -38,4 +38,40 @@ class ConfigTest extends PHPUnit_Framework_TestCase
         $this->config->load(__DIR__ . '/config/variable.yml');
         $this->assertEquals('bar', $this->config->get('name'));
     }
+
+    public function testSetConfig()
+    {
+        $this->config->set('a', 'hello');
+        $this->config->set('a.b.c', 'world');
+        $this->assertEquals([
+            'a' => [
+                'hello',
+                'b' => [
+                    'c' => 'world'
+                ],
+            ],
+        ], $this->config->all());
+        $this->config->set('a.b', 'world');
+        $this->assertEquals([
+            'a' => [
+                'hello',
+                'b' => 'world',
+            ],
+        ], $this->config->all());
+    }
+
+    public function testHasConfig()
+    {
+        $this->config->set('a', [
+            'b' => [
+                'c' => 'hello world',
+            ],
+        ]);
+        $this->assertSame(true, $this->config->has('a'));
+        $this->assertSame(true, $this->config->has('a.b'));
+        $this->assertSame(true, $this->config->has('a.b.c'));
+        $this->assertSame(false, $this->config->has('a.b.cd'));
+        $this->assertSame(false, $this->config->has('b'));
+        $this->assertSame(false, $this->config->has('b.c'));
+    }
 }
